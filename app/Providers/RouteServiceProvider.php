5ff1ae94e485f75bdb5b45804c8f1939e25dd10b
JabosCOL <85903037+JabosCOL@users.noreply.github.com>
2022-04-28 19:59:18 -0500

    Añadiendo plantilla inicial

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,6 +46,11 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            // Se agrega el archivo admin.php para que sea reconocido como archivo de rutas
+            Route::middleware('web', 'auth')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/admin.php'));
         });
     }
 
